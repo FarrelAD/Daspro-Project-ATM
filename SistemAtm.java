828b33246d14ce58d0d0a21fe27d5f6445feab57
sukinnamz <innama.maesaputri@gmail.com>
2023-09-10 18:41:42 +0700

    auth user no array

--- SistemAtm.java
+++ SistemAtm.java
@@ -1,5 +1,28 @@
-public class SistemAtm {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class SistemAtm {
+
+    public static void main(String[] args) {
+        String no_rek = "innama";
+        String pin = "1234";
+
+        System.out.println("********************");
+        System.out.println("* Sistem mesin ATM *");
+        System.out.println("********************");
+
+        System.out.println("Masukkan nomor rekening :");
+        Scanner scanner = new Scanner(System.in);
+        String input_no_rek = scanner.nextLine();
+
+        System.out.println("Masukkan pin anda :");
+        String input_pin = scanner.nextLine();
+
+        if (input_no_rek.equals(no_rek) && input_pin.equals(pin)) {
+            System.out.println("Berhasil");
+        } else {
+            System.out.println("Gagal");
+        }
+
+        scanner.close();
     }
 }

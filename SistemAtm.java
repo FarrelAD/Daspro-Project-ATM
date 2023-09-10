0978679898b2143e6f0f06c2541aa7d09cc92bb7
sukinnamz <innama.maesaputri@gmail.com>
2023-09-10 18:51:40 +0700

    auth user while no array

--- SistemAtm.java
+++ SistemAtm.java
@@ -3,26 +3,29 @@ import java.util.Scanner;
 public class SistemAtm {
 
     public static void main(String[] args) {
-        String no_rek = "innama";
-        String pin = "1234";
+        while (true) {
+            String no_rek = "innama";
+            String pin = "1234";
 
-        System.out.println("********************");
-        System.out.println("* Sistem mesin ATM *");
-        System.out.println("********************");
+            System.out.println("**************************");
+            System.out.println("*                        *");
+            System.out.println("*    Sistem mesin ATM    *");
+            System.out.println("*                        *");
+            System.out.println("**************************");
 
-        System.out.println("Masukkan nomor rekening :");
-        Scanner scanner = new Scanner(System.in);
-        String input_no_rek = scanner.nextLine();
+            System.out.println("Masukkan nomor rekening :");
+            Scanner scanner = new Scanner(System.in);
+            String input_no_rek = scanner.nextLine();
 
-        System.out.println("Masukkan pin anda :");
-        String input_pin = scanner.nextLine();
+            System.out.println("Masukkan pin anda :");
+            String input_pin = scanner.nextLine();
 
-        if (input_no_rek.equals(no_rek) && input_pin.equals(pin)) {
-            System.out.println("Berhasil");
-        } else {
-            System.out.println("Gagal");
+            if (input_no_rek.equals(no_rek) && input_pin.equals(pin)) {
+                System.out.println("Berhasil");
+            } else {
+                System.out.println("Gagal");
+            }
+
         }
-
-        scanner.close();
     }
 }

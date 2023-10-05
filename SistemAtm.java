125676f438720d6af86ef712bde2991928d4a646
sukinnamz <innama.maesaputri@gmail.com>
2023-10-05 19:27:38 +0700

    menu transfer

--- SistemAtm.java
+++ SistemAtm.java
@@ -31,7 +31,48 @@ public class SistemAtm {
             }
 
             if (input_no_rek.equals(no_rek) && input_pin.equals(pin) && status.equals("aman")) {
-                System.out.println("Berhasil login");
+                String no_rek_tujuan;
+                int nom_transfer, saldo_awal = 5000000, sisa_saldo;
+                System.out.println("Anda berhasil login, silahkan memilih menu dibawah ini :");
+                System.out.println("1. Transfer");
+                System.out.println("2. Tarik tunai");
+                System.out.println("3. Setor tunai");
+                System.out.println("4. Pembayaran lain-lain");
+                System.out.println("Menu yang dipilih :");
+                int menu = scanner.nextInt();
+
+                System.out.println("**************************");
+
+                switch (menu) {
+                    case 1:
+                        Scanner scanner2 = new Scanner(System.in);
+                        System.out.println("Anda memilih menu transfer");
+                        System.out.println("**************************");
+                        System.out.println("Masukkan nomor rekening tujuan :");
+                        no_rek_tujuan = scanner2.nextLine();
+                        System.out.println("Masukkan nominal transfer : ");
+                        nom_transfer = scanner.nextInt();
+                        if (nom_transfer > saldo_awal) {
+                            System.out.println("Transaksi gagal, periksa kembali saldo anda");
+                        } else {
+                            sisa_saldo = saldo_awal - nom_transfer;
+                            System.out.println("Transfer ke nomor " + no_rek_tujuan + " berhasil dilakukan");
+                            System.out.println("Sisa saldo anda : " + sisa_saldo);
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Anda memilih menu tarik tunai");
+                        break;
+                    case 3:
+                        System.out.println("Anda memilih menu setor tunai");
+                        break;
+                    case 4:
+                        System.out.println("Anda memilih menu pembayaran lain lain");
+                        break;
+                    default:
+                        System.out.println("Periksa kembali inputan anda");
+                        break;
+                }
                 break; // Successful login, exit the loop
             } else {
                 System.out.println("Gagal login, periksa kembali detail anda");

205c71bcd7746fee502c11cb2b2776e6e1427884
Farrel Augusta Dinata <fadfarrel@gmail.com>
2023-11-21 19:25:48 +0700

    add file Function000SistemATM.java

---
+++ Function000SistemATM.java
@@ -0,0 +1,54 @@
+public class Function000SistemATM {
+    public static void main(String[] args) {
+        TryToLogin();
+        Menu();
+        Transfer();
+        TarikTunai();
+        SetorTunai();
+        PembayaranLainnya();
+        CekSaldo();
+        UbahPin();
+        Exit();
+    }
+
+    public static void TryToLogin() {
+
+    }
+
+    public static void Menu() {
+
+    }
+
+    public static void Transfer() {
+
+    }
+
+    public static void TarikTunai() {
+
+    }
+
+    public static void SetorTunai() {
+
+    }
+
+    public static void PembayaranLainnya() {
+
+    }
+
+    public static void RiwayatTransaksi() {
+
+    }
+
+    public static void CekSaldo() {
+
+    }
+
+    public static void UbahPin() {
+
+    }
+
+    public static void Exit() {
+
+    }
+    
+}

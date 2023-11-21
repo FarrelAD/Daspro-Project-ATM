81d0e8a223edbed1e6985abcc0239457da71a170
Farrel Augusta Dinata <140877757+FarrelAD@users.noreply.github.com>
2023-11-21 08:51:07 +0700

    fix: penamaan kelas utama sesuai dengan nama file

--- SistemATM.java
+++ SistemATM.java
@@ -4,7 +4,7 @@ import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class SistemAtm {
+public class SistemATM {
     public static void main(String[] args) {
 
         Scanner scanner1 = new Scanner(System.in);

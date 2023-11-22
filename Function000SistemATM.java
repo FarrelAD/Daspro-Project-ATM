f9e0bf65d5f447811fd8cc35d291de98b845b668
sukinnamz <innama.maesaputri@gmail.com>
2023-11-22 10:00:27 +0700

    ADD : Inisialisasi dan deklarasi variable global

--- Function000SistemATM.java
+++ Function000SistemATM.java
@@ -1,4 +1,35 @@
+
+// Program sistem ATM - Kelompok 4 - TI-1B
+// Fitur yang tersedia: autentifikasi pengguna, transfer, tarik tunai, setor tunai, pembayaran lain-lain, riwayat transaksi, cek saldo, ubah PIN, dan EXIT
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Function000SistemATM {
+    static Scanner scanner1 = new Scanner(System.in);
+    static Scanner scanner2 = new Scanner(System.in);
+    static Scanner scanner3 = new Scanner(System.in);
+    static Scanner scanner4 = new Scanner(System.in);
+    static Scanner scanner5 = new Scanner(System.in);
+    static Scanner scannerTF = new Scanner(System.in);
+
+    // inisialisasi dan deklarasi variabel yang dibutuhkan
+    static String[][] accountData = {
+            { "1234567", "1234", "7000000", "aman" },
+            { "7654321", "5678", "4000000", "aman" },
+            { "7777777", "7777", "10000000", "aman" },
+            { "0000000", "0000", "900000000", "aman" }
+    };
+    static int maxTransactionHistory = 10, transactionCount = 10,
+            accountLineIndex = 0, loginAttempts = 1; // variabel penunjang pencatatan riwayat transaksi dan
+                                                     // counter percobaan login
+    static String[] transactionHistory = new String[maxTransactionHistory]; // array riwayat transaksi
+    static boolean isAccountValid = false,
+            isTargetAccountValid = false; // variabel autentikasi login dan validasi nomor rekening
+    static final int MAX_LOGIN_ATTEMPTS = 3; // maksimal login yang bisa dilakukan pengguna
+    static boolean isTransactionExit = true;
+    static String pressEnter;
+
     public static void main(String[] args) {
         TryToLogin();
         Menu();
@@ -50,5 +81,5 @@ public class Function000SistemATM {
     public static void Exit() {
 
     }
-    
+
 }
